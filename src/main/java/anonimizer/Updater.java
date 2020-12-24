2280c6f45abaec93b6038cf0d77e696c325fb5c8
ilya karkin <karkinia@yandex.ru>
2020-12-24 18:19:46 +0300

    Scripted auto-commit on change (2020-12-24 18:19:46) by gitwatch.sh

--- src/main/java/anonimizer/Updater.java
+++ src/main/java/anonimizer/Updater.java
@@ -1,4 +1,23 @@
 package anonimizer;
 
+import java.util.ArrayList;
+
 public class Updater {
+    private ArrayList<String> servers;
+
+    public Updater(ArrayList<String> servers) {
+        this.servers = servers;
+    }
+
+    public Updater() {
+        this.servers = new ArrayList<>();
+    }
+
+    public ArrayList<String> getServers() {
+        return servers;
+    }
+
+    public void setServers(ArrayList<String> servers) {
+        this.servers = servers;
+    }
 }
