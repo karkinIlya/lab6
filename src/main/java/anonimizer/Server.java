49fcf0bb1697f6e1a2c477989433842afd4eec76
ilya karkin <karkinia@yandex.ru>
2020-12-24 18:33:19 +0300

    Scripted auto-commit on change (2020-12-24 18:33:19) by gitwatch.sh

--- src/main/java/anonimizer/Server.java
+++ src/main/java/anonimizer/Server.java
@@ -4,27 +4,36 @@ import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.http.impl.engine.client.PoolConductor;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.javadsl.Flow;
 
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 
+import static akka.http.javadsl.server.Directives.completeWithFuture;
+import static akka.http.javadsl.server.Directives.parameter;
 import static jdk.internal.jline.TerminalFactory.get;
 
 public class Server {
 
-    public static Route createRoute() {
-        return Route(get(() ->
-                        parameter("url", url ->
+    public static final String URL_PARAM = "url";
+    public static final String COUNT_PARAM = "count";
 
+    public static Route createRoute(Http http, ActorRef confActor) {
+        return Route(get(() ->
+                        parameter(URL_PARAM, url ->
+                                parameter(COUNT_PARAM, count -> {
+                                    return Integer.parseInt(count) <= 0 ?
+                                            completeWithFuture(http.singleRequest(HttpRequest.create(url))) :
+                                            completeWithFuture(Patterns.ask(confActor, new))
+                                }
                         )
 
                 )
-        )
+        );
     }
 
     public static void main(String[] argv) {
@@ -35,7 +44,7 @@ public class Server {
 
         // init zookeeper
 
-//        final Flow<HttpRequest, HttpResponse, NotUsed> route = createRoute().flow();
+        final Flow<HttpRequest, HttpResponse, NotUsed> route = createRoute(http, confActor).flow();
         final CompletionStage<> binding = http.bindAndHandle(
                 flow,
                 connection,
