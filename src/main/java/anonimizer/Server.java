dc2eb54a997b18921f18cc9fb1385ef4eb92ce5e
ilya karkin <karkinia@yandex.ru>
2020-12-24 18:05:56 +0300

    Scripted auto-commit on change (2020-12-24 18:05:55) by gitwatch.sh

--- src/main/java/anonimizer/Server.java
+++ src/main/java/anonimizer/Server.java
@@ -1,9 +1,20 @@
 package anonimizer;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+
+import java.net.http.HttpRequest;
 
 public class Server {
     public static void main(String[] argv) {
-        ActorSystem system = ActorSystem
+        ActorSystem system = ActorSystem.create("routes");
+        Http http = Http.get(system);
+        ActorRef confActor = system.actorOf(Props.create(ConfActor.class));
+        int PORT = Integer.parseInt(argv[0]);
+
+        final Flow<HttpRequest, HttpResponce> route = createRoute().flow();
+
     }
 }
