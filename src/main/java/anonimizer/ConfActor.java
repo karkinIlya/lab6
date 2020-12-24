bafb29c6cbec6839d73cc19bbfc8dc49002920e8
ilya karkin <karkinia@yandex.ru>
2020-12-24 18:15:15 +0300

    Scripted auto-commit on change (2020-12-24 18:15:15) by gitwatch.sh

--- src/main/java/anonimizer/ConfActor.java
+++ src/main/java/anonimizer/ConfActor.java
@@ -1,4 +1,23 @@
 package anonimizer;
 
-public class ConfActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.ArrayList;
+
+public class ConfActor extends AbstractActor {
+    ArrayList<String> servers = new ArrayList<String>();
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(
+                        Updater.class, message -> {
+                            servers.add
+                        }
+                )
+                .match(
+
+                )
+    }
 }
