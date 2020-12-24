0ed26e18cf00844f30ec54661c70745552711aaa
ilya karkin <karkinia@yandex.ru>
2020-12-24 18:33:59 +0300

    Scripted auto-commit on change (2020-12-24 18:33:59) by gitwatch.sh

--- src/main/java/anonimizer/Server.java
+++ src/main/java/anonimizer/Server.java
@@ -28,7 +28,7 @@ public class Server {
                                 parameter(COUNT_PARAM, count -> {
                                     return Integer.parseInt(count) <= 0 ?
                                             completeWithFuture(http.singleRequest(HttpRequest.create(url))) :
-                                            completeWithFuture(Patterns.ask(confActor, ))
+                                            completeWithFuture(Patterns.ask(confActor, new ServerSelector()))
                                 }
                         )
 
