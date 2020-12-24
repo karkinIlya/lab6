bffa2dfa24712f9e4ccf3b5102f20b652bdc476c
ilya karkin <karkinia@yandex.ru>
2020-12-24 18:17:00 +0300

    Scripted auto-commit on change (2020-12-24 18:17:00) by gitwatch.sh

--- src/main/java/anonimizer/ConfActor.java
+++ src/main/java/anonimizer/ConfActor.java
@@ -6,18 +6,20 @@ import akka.japi.pf.ReceiveBuilder;
 import java.util.ArrayList;
 
 public class ConfActor extends AbstractActor {
-    ArrayList<String> servers = new ArrayList<String>();
+    ArrayList<String> servers = new ArrayList<>();
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(
                         Updater.class, message -> {
-                            servers.add
+                            servers.add(message);
                         }
                 )
                 .match(
-
-                )
+                        ServerSelector.class, message -> {
+                            //
+                        }
+                ).build();
     }
 }
