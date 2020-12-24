e16d8ddcfb8974887bcdd26d05aee2f20a49ca03
ilya karkin <karkinia@yandex.ru>
2020-12-24 18:41:41 +0300

    Scripted auto-commit on change (2020-12-24 18:41:41) by gitwatch.sh

--- src/main/java/anonimizer/Server.java
+++ src/main/java/anonimizer/Server.java
@@ -37,7 +37,7 @@ public class Server {
                                                             port -> (String)port
                                                     ).thenCompose(
                                                             port -> http.singleRequest(HttpRequest.create(
-                                                                    "https://localhost:" + url
+                                                                    "https://localhost:" + url + "?url="
                                                             ))
                                                     )
                                 }
